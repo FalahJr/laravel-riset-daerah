c11b715ea267a93a2b1d92ae62b7ec2803399057
Khoiru Miftakhul Falah <mrcr4zysm1le@gmail.com>
2024-06-04 03:36:25 +0700

    Feat (frontend) : slicing crud topik riset

---
+++ resources/views/pages/edit-topik-riset.blade.php
@@ -0,0 +1,130 @@
+@extends('layouts.app')
+
+@section('title', 'Edit Topik Riset')
+
+@push('style')
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="{{ asset('library/summernote/dist/summernote-bs4.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/codemirror/lib/codemirror.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/codemirror/theme/duotone-dark.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/selectric/public/selectric.css') }}">
+@endpush
+
+@section('main')
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header">
+                <h1>Edit Topik Riset</h1>
+                <div class="section-header-breadcrumb">
+                    <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                    <div class="breadcrumb-item"><a href="#">Topik Riset</a></div>
+                    <div class="breadcrumb-item">Edit Topik Riset</div>
+                </div>
+            </div>
+
+            <div class="section-body">
+
+
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>Form Edit Topik Riset</h4>
+                            </div>
+                            <form class="form" action="/admin/topik-riset/{{ Request::segment(3) }}" method="post"
+                                enctype="multipart/form-data">
+                                @csrf
+                                @method('PUT')
+
+                                <div class="card-body">
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Judul</label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <input type="text" class="form-control" name="judul" required
+                                                value="{{ $topik_riset->judul }}">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Tahun</label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <input type="number" class="form-control" name="tahun" required
+                                                min="0" value="{{ $topik_riset->tahun }}">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">No
+                                            Telepon</label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <input type="number" class="form-control" name="no_telepon" required
+                                                min="0" value="{{ $topik_riset->no_telepon }}">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Abstrak</label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <textarea class="summernote" name="abstrak" required>{{ $topik_riset->abstrak }}</textarea>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">File</label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <a href="{{ asset('file_upload/topik-riset/' . $topik_riset->upload_file) }}"
+                                                class="btn btn-primary btn-md mb-3" target="_blank">
+                                                View File
+                                            </a>
+                                            <input type="file" class="form-control" name="upload_file">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Status
+                                            Publikasi</label>
+                                        <div class="col-sm-12 col-md-7">
+
+                                            <select class="form-control select2" name="is_publish" required>
+                                                <option value="" disabled selected>Pilih Status</option>
+
+                                                <option value="Y"
+                                                    {{ $topik_riset->is_publish == 'Y' ? 'selected' : '' }}>Publikasi
+                                                </option>
+                                                <option value="N"
+                                                    {{ $topik_riset->is_publish == 'N' ? 'selected' : '' }}>Privasi
+                                                </option>
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <button class="btn btn-primary" type="submit">Publish</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </section>
+    </div>
+@endsection
+
+@push('scripts')
+    <!-- JS Libraies -->
+    <script src="{{ asset('library/summernote/dist/summernote-bs4.js') }}"></script>
+    {{-- <script src="{{ asset('library/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js') }}"></script> --}}
+
+    <script src="{{ asset('library/codemirror/lib/codemirror.js') }}"></script>
+    <script src="{{ asset('library/codemirror/mode/javascript/javascript.js') }}"></script>
+    <script src="{{ asset('library/selectric/public/jquery.selectric.min.js') }}"></script>
+
+    {{-- <script src="{{ asset('library/upload-preview/upload-preview.js') }}"></script> --}}
+
+    <!-- Page Specific JS File -->
+    {{-- <script src="{{ asset('js/page/features-post-create.js') }}"></script> --}}
+
+    <!-- Page Specific JS File -->
+@endpush
---
+++ resources/views/pages/manage-topik-riset.blade.php
@@ -0,0 +1,134 @@
+@extends('layouts.app')
+
+@section('title', 'Management Topik Riset')
+
+@push('style')
+    <!-- CSS Libraries -->
+@endpush
+<?php
+use Illuminate\Support\Str;
+
+?>
+
+@section('main')
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header">
+                <h1>Management Topik Riset</h1>
+                <div class="section-header-breadcrumb">
+                    <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                    <div class="breadcrumb-item"><a href="#">Topik Riset</a></div>
+
+                </div>
+            </div>
+
+            <div class="section-body">
+
+                <div class="row">
+
+                    <div class="col-12 ">
+                        <a href="{{ url('admin/topik-riset/create') }}" class="btn btn-success btn-block w-25 ">+ Tambah
+                            Topik
+                            Riset</a>
+                        <div class="card mt-4">
+
+
+                            <div class="card-body p-0">
+                                <div class="table-responsive">
+
+                                    <table class="table-striped table-md table">
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Judul</th>
+                                            <th>Tahun</th>
+                                            <th>No.Telepon</th>
+                                            <th>Dokumen</th>
+                                            <th>Status</th>
+                                            <th>Action</th>
+                                        </tr>
+                                        <?php $no = 1; ?>
+
+                                        @foreach ($data as $list)
+                                            <tr>
+                                                <td>{{ $no }}</td>
+
+                                                <td>{{ $list->judul }}</td>
+                                                <td>{{ $list->tahun }}</td>
+                                                <td>{{ $list->no_telepon }}</td>
+
+                                                {{-- <td>
+                                                    {!! nl2br(htmlspecialchars_decode(Str::limit($list->deskripsi, 1000))) !!}
+                                                </td> --}}
+
+                                                <td>
+                                                    <a href="{{ asset('file_upload/topik-riset/' . $list->upload_file) }}"
+                                                        class="btn btn-primary btn-sm" download>
+                                                        Download
+                                                    </a>
+                                                </td>
+                                                <td>
+                                                    @if ($list->is_publish == 'Y')
+                                                        <span class="badge badge-success w-75">Publikasi</span>
+                                                    @else
+                                                        <span class="badge badge-danger w-75">Tidak Dipublikasi</span>
+                                                    @endif
+
+                                                </td>
+                                                <td>
+                                                    <div class="d-flex">
+
+                                                        <a href="topik-riset/{{ $list->id }}/edit"
+                                                            class="btn btn-info mr-2">Detail</a>
+                                                        <form class="" method="POST"
+                                                            action="/admin/topik-riset/{{ $list->id }}"
+                                                            style="display:inline;">
+                                                            @csrf
+
+                                                            @method('DELETE')
+                                                            <button class="btn btn-danger">Hapus</button>
+                                                        </form>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                            <?php $no++; ?>
+                                        @endforeach
+
+
+                                    </table>
+                                </div>
+                            </div>
+                            {{-- <div class="card-footer text-right">
+                                <nav class="d-inline-block">
+                                    <ul class="pagination mb-0">
+                                        <li class="page-item disabled">
+                                            <a class="page-link" href="#" tabindex="-1"><i
+                                                    class="fas fa-chevron-left"></i></a>
+                                        </li>
+                                        <li class="page-item active"><a class="page-link" href="#">1 <span
+                                                    class="sr-only">(current)</span></a></li>
+                                        <li class="page-item">
+                                            <a class="page-link" href="#">2</a>
+                                        </li>
+                                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                        <li class="page-item">
+                                            <a class="page-link" href="#"><i class="fas fa-chevron-right"></i></a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div> --}}
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    </div>
+@endsection
+
+@push('scripts')
+    <!-- JS Libraies -->
+    <script src="{{ asset('library/jquery-ui-dist/jquery-ui.min.js') }}"></script>
+
+    <!-- Page Specific JS File -->
+    <script src="{{ asset('js/page/components-table.js') }}"></script>
+@endpush
